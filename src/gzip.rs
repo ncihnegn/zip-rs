e213e9b427d1e0230f0981a3f290125617aafa61
ncihnegn <ningchen@outlook.com>
2017-02-04 03:08:42 -0800

    Add support of multi-member gzip

--- src/gzip.rs
+++ src/gzip.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 use std::fs::File;
-use std::io::{BufReader, BufWriter, Error, ErrorKind};
+use std::io::{BufReader, BufWriter, Error, ErrorKind, SeekFrom};
 use std::io::prelude::*;
 use std::str;
 
@@ -68,98 +68,106 @@ impl fmt::Display for OS {
 }
 
 #[allow(dead_code)]
-struct GzipMember {
+pub struct GzipMember {
     flg: Flags,
     xfl: ExtraFlags,
     mtime: u32,
     os: OS,
     crc16: u16,
+    offset: u64,
     crc32: u32,
     isize: u32,
 }
 
-pub fn parse(file_name: &str) -> Result<(), Error> {
+pub fn parse(file_name: &str) -> Result<Vec<GzipMember>, Error> {
     let file = try!(File::open(file_name));
     let mut reader = BufReader::new(file);
     let mut byte: [u8; 1] = [0; 1];
     let mut word: [u8; 2] = [0; 2];
     let mut dword: [u8; 4] = [0; 4];
-    try!(reader.read_exact(&mut byte));
-    assert!(byte[0] == 0x1F);
-    try!(reader.read_exact(&mut byte));
-    assert!(byte[0] == 0x8B);
-    try!(reader.read_exact(&mut byte));
-    assert!(byte[0] == 8);//Deflate Only
-    try!(reader.read_exact(&mut byte));
-    let mut flg = Flags { ftext: false, fhcrc: false, fextra: false, fname: false, fcomment: false };
-    if byte[0] & 1 == 1 {
-        flg.ftext = true;
-    }
-    if byte[0] & 2 == 2 {
-        flg.fhcrc = true;
-    }
-    if byte[0] & 4 == 4 {
-        flg.fextra = true;
-    }
-    if byte[0] & 8 == 8 {
-        flg.fname = true;
-    }
-    if byte[0] & 16 == 16 {
-        flg.fcomment = true;
-    }
-    try!(reader.read_exact(&mut dword));
-    let mtime = trans32(dword);
-    try!(reader.read_exact(&mut byte));
-    let xfl = match ExtraFlags::from_u8(byte[0]) {
-        Some(x) => x,
-        None => return Err(Error::new(ErrorKind::Other, "Bad XFL")),
-    };
+    let mut members = Vec::new();
+    let end = reader.seek(SeekFrom::End(0)).unwrap();
+    let _ = reader.seek(SeekFrom::Start(0));
+    while reader.seek(SeekFrom::Current(0)).unwrap() != end {
+        try!(reader.read_exact(&mut byte));
+        assert!(byte[0] == 0x1F);
+        try!(reader.read_exact(&mut byte));
+        assert!(byte[0] == 0x8B);
+        try!(reader.read_exact(&mut byte));
+        assert!(byte[0] == 8);//Deflate Only
+        try!(reader.read_exact(&mut byte));
+        let mut flg = Flags { ftext: false, fhcrc: false, fextra: false, fname: false, fcomment: false };
+        if byte[0] & 1 == 1 {
+            flg.ftext = true;
+        }
+        if byte[0] & 2 == 2 {
+            flg.fhcrc = true;
+        }
+        if byte[0] & 4 == 4 {
+            flg.fextra = true;
+        }
+        if byte[0] & 8 == 8 {
+            flg.fname = true;
+        }
+        if byte[0] & 16 == 16 {
+            flg.fcomment = true;
+        }
+        try!(reader.read_exact(&mut dword));
+        let mtime = trans32(dword);
+        try!(reader.read_exact(&mut byte));
+        let xfl = match ExtraFlags::from_u8(byte[0]) {
+            Some(x) => x,
+            None => return Err(Error::new(ErrorKind::Other, "Bad XFL")),
+        };
 
-    try!(reader.read_exact(&mut byte));
-    let os = match OS::from_u8(byte[0]) {
-        Some(x) => x,
-        None => return Err(Error::new(ErrorKind::Other, "Bad XFL")),
-    };
-    let mut extra = Vec::<u8>::new();
-    if flg.fextra {
-        try!(reader.read_exact(&mut word));
-        let xlen = trans16(word);
-        extra.resize(xlen as usize, 0);
-        try!(reader.read_exact(&mut extra as &mut [u8]));
-    }
-    let mut file_name = Vec::<u8>::new();
-    if flg.fname {
-        try!(reader.read_until(0, &mut file_name));
-    }
-    debug!("File name: {}", str::from_utf8(&file_name).unwrap());
-    let mut file_comment = Vec::<u8>::new();
-    if flg.fcomment {
-        try!(reader.read_until(0, &mut file_comment));
-        debug!("File comment: {}", str::from_utf8(&file_comment).unwrap());
-    }
-    let mut crc16: u16 = 0;
-    if flg.fhcrc {
-        try!(reader.read_exact(&mut word));
-        crc16 = trans16(word);
-    }
-    let out = Vec::<u8>::new();
-    let mut writer = BufWriter::new(out);
-    let (decompressed_size, crc) = try!(inflate(&mut reader, &mut writer));
-    try!(reader.read_exact(&mut dword));
-    let out = match writer.into_inner() {
-        Ok(x) => x,
-        Err(_) => return Err(Error::new(ErrorKind::Other, "Can't get the inner output")),
-    };
-    let crc32: u32 = trans32(dword);
-    try!(reader.read_exact(&mut dword));
-    let isize: u32 = trans32(dword);
-    debug!("{}({:08x}), expected {}({:08x})", decompressed_size, crc, isize, crc32);
-    assert_eq!(decompressed_size, isize);
-    assert_eq!(crc, crc32);
-    debug!("\n{}", str::from_utf8(&out).unwrap());
+        try!(reader.read_exact(&mut byte));
+        let os = match OS::from_u8(byte[0]) {
+            Some(x) => x,
+            None => return Err(Error::new(ErrorKind::Other, "Bad XFL")),
+        };
+        let mut extra = Vec::<u8>::new();
+        if flg.fextra {
+            try!(reader.read_exact(&mut word));
+            let xlen = trans16(word);
+            extra.resize(xlen as usize, 0);
+            try!(reader.read_exact(&mut extra as &mut [u8]));
+        }
+        let mut file_name = Vec::<u8>::new();
+        if flg.fname {
+            try!(reader.read_until(0, &mut file_name));
+        }
+        debug!("File name: {}", str::from_utf8(&file_name).unwrap());
+        let mut file_comment = Vec::<u8>::new();
+        if flg.fcomment {
+            try!(reader.read_until(0, &mut file_comment));
+            debug!("File comment: {}", str::from_utf8(&file_comment).unwrap());
+        }
+        let mut crc16: u16 = 0;
+        if flg.fhcrc {
+            try!(reader.read_exact(&mut word));
+            crc16 = trans16(word);
+        }
+        let offset = reader.seek(SeekFrom::Current(0)).unwrap();
+        let out = Vec::<u8>::new();
+        let mut writer = BufWriter::new(out);
+        let (decompressed_size, crc) = try!(inflate(&mut reader, &mut writer));
+        try!(reader.read_exact(&mut dword));
+        let out = match writer.into_inner() {
+            Ok(x) => x,
+            Err(_) => return Err(Error::new(ErrorKind::Other, "Can't get the inner output")),
+        };
+        let crc32: u32 = trans32(dword);
+        try!(reader.read_exact(&mut dword));
+        let isize: u32 = trans32(dword);
+        debug!("{}({:08x}), expected {}({:08x})", decompressed_size, crc, isize, crc32);
+        assert_eq!(decompressed_size, isize);
+        assert_eq!(crc, crc32);
+        debug!("\n{}", str::from_utf8(&out).unwrap());
 
-    let _ = GzipMember { flg: flg, mtime: mtime, xfl: xfl, os: os, crc16: crc16, crc32: crc32, isize: isize };
-    Ok(())
+        let mem = GzipMember { flg: flg, mtime: mtime, xfl: xfl, os: os, crc16: crc16, crc32: crc32, isize: isize, offset: offset };
+        members.push(mem);
+    }
+    Ok(members)
 }
 
 #[cfg(test)]
@@ -169,5 +177,10 @@ mod test {
     #[test]
     fn basic() {
         assert!(parse("test/dynamic_huffman.gz").is_ok());
+    }
+
+    #[test]
+    fn multiple() {
+        assert!(parse("test/multiple.gz").is_ok());
     }
 }
